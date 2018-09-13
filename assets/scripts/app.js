9b5be57dfe263970ed488c7257da56805096c817
Chris Thomas <webmaster102@gmail.com>
2018-09-12 23:30:37 -0400

    moved daily script to app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,29 @@
+var today = new Date(),
+  days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+switch(days[today.getDay()]) {
+  case "Sunday":
+    document.getElementById("sun").style.display = "block";
+    break;
+  case "Monday":
+    document.getElementById("mon").style.display = "block";
+    break;
+  case "Tuesday":
+    document.getElementById("tues").style.display = "block";
+    break;
+  case "Wednesday":
+    document.getElementById("wed").style.display = "block";
+    break;
+  case "Thursday":
+    document.getElementById("thurs").style.display = "block";
+    break;
+  case "Friday":
+    document.getElementById("fri").style.display = "block";
+    break;
+  case "Saturday":
+    document.getElementById("sat").style.display = "block";
+    break;
+}
+
 if('serviceWorker' in navigator) {
 	navigator.serviceWorker
 		.register('./service-worker.js', { scope: './' })
